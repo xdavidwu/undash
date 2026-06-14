97772ebc39aac374f173200ebee60edd229d835f
Pinghao Wu <xdavidwuph@gmail.com>
2026-06-14 16:42:01 +0800

    feat: support all verber mapped types on single resource

--- main.go
+++ main.go
@@ -16,6 +16,7 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/runtime/schema"
 
 	undashctx "github.com/xdavidwu/undash/internal/context"
 	"github.com/xdavidwu/undash/internal/dashboard"
@@ -35,6 +36,11 @@ type kindMeta struct {
 	listKind string
 }
 
+type verberMappedMeta struct {
+	singular   string
+	namespaced bool
+}
+
 var (
 	coreNamespacedResources = map[string]kindMeta{
 		"services": {
@@ -42,6 +48,149 @@ var (
 			singular: "service",
 			listKind: "ServiceList",
 		},
+	}
+
+	verberClientTypeDefaultGV       = schema.GroupVersion{Group: "", Version: "v1"}
+	verberClientTypeAppsGV          = schema.GroupVersion{Group: "apps", Version: "v1"}
+	verberClientTypeAutoscalingGV   = schema.GroupVersion{Group: "autoscaling", Version: "v1"}
+	verberClientTypeNetworkingGV    = schema.GroupVersion{Group: "networking.k8s.io", Version: "v1"}
+	verberClientTypeBatchGV         = schema.GroupVersion{Group: "batch", Version: "v1"}
+	verberClientTypeBetaBatchGV     = schema.GroupVersion{Group: "batch", Version: "v1beta1"}
+	verberClientTypeAPIExtensionsGV = schema.GroupVersion{Group: "apiextensions.k8s.io", Version: "v1"}
+	verberClientTypeStorageGV       = schema.GroupVersion{Group: "storage.k8s.io", Version: "v1"}
+	verberClientTypeRbacGV          = schema.GroupVersion{Group: "rbac.authorization.k8s.io", Version: "v1"}
+	// dashboard extensions custom resource
+	verberClientTypePluginsGV = schema.GroupVersion{Group: "dashboard.k8s.io", Version: "v1alpha1"}
+
+	// v2 has its name => client mapping and parse CRD for custom resources,
+	// while v3 seems to rely on discovery + dynamic client
+	verberMappedResources = map[schema.GroupVersionResource]verberMappedMeta{
+		verberClientTypeDefaultGV.WithResource("configmaps"): {
+			singular:   "configmap",
+			namespaced: true,
+		},
+		verberClientTypeAppsGV.WithResource("daemonsets"): {
+			singular:   "daemonset",
+			namespaced: true,
+		},
+		verberClientTypeAppsGV.WithResource("deployments"): {
+			singular:   "deployment",
+			namespaced: true,
+		},
+		verberClientTypeDefaultGV.WithResource("events"): {
+			singular:   "event",
+			namespaced: true,
+		},
+		verberClientTypeAutoscalingGV.WithResource("horizontalpodautoscalers"): {
+			singular:   "horizontalpodautoscaler",
+			namespaced: true,
+		},
+		verberClientTypeNetworkingGV.WithResource("ingresses"): {
+			singular:   "ingress",
+			namespaced: true,
+		},
+		verberClientTypeNetworkingGV.WithResource("ingressclasses"): {
+			singular:   "ingressclass",
+			namespaced: false,
+		},
+		verberClientTypeBatchGV.WithResource("jobs"): {
+			singular:   "job",
+			namespaced: true,
+		},
+		verberClientTypeBetaBatchGV.WithResource("cronjobs"): {
+			singular:   "cronjob",
+			namespaced: true,
+		},
+		// XXX singular on dashboard map?
+		verberClientTypeDefaultGV.WithResource("limitranges"): {
+			singular:   "limitrange",
+			namespaced: true,
+		},
+		verberClientTypeDefaultGV.WithResource("namespaces"): {
+			singular:   "namespace",
+			namespaced: false,
+		},
+		verberClientTypeDefaultGV.WithResource("nodes"): {
+			singular:   "node",
+			namespaced: false,
+		},
+		verberClientTypeDefaultGV.WithResource("persistentvolumeclaims"): {
+			singular:   "persistentvolumeclaim",
+			namespaced: true,
+		},
+		verberClientTypeDefaultGV.WithResource("persistentvolumes"): {
+			singular:   "persistentvolume",
+			namespaced: false,
+		},
+		verberClientTypeAPIExtensionsGV.WithResource("customresourcedefinitions"): {
+			singular:   "customresourcedefinition",
+			namespaced: false,
+		},
+		verberClientTypeDefaultGV.WithResource("pods"): {
+			singular:   "pod",
+			namespaced: true,
+		},
+		verberClientTypeAppsGV.WithResource("replicasets"): {
+			singular:   "replicaset",
+			namespaced: true,
+		},
+		verberClientTypeDefaultGV.WithResource("replicationcontrollers"): {
+			singular:   "replicationcontroller",
+			namespaced: true,
+		},
+		verberClientTypeDefaultGV.WithResource("resourcequotas"): {
+			singular:   "resourcequota",
+			namespaced: true,
+		},
+		verberClientTypeDefaultGV.WithResource("secrets"): {
+			singular:   "secret",
+			namespaced: true,
+		},
+		verberClientTypeDefaultGV.WithResource("services"): {
+			singular:   "service",
+			namespaced: true,
+		},
+		verberClientTypeDefaultGV.WithResource("serviceaccounts"): {
+			singular:   "serviceaccount",
+			namespaced: true,
+		},
+		verberClientTypeAppsGV.WithResource("statefulsets"): {
+			singular:   "statefulset",
+			namespaced: true,
+		},
+		verberClientTypeStorageGV.WithResource("storageclasses"): {
+			singular:   "storageclass",
+			namespaced: false,
+		},
+		verberClientTypeDefaultGV.WithResource("endpoints"): {
+			singular:   "endpoint",
+			namespaced: true,
+		},
+		verberClientTypeNetworkingGV.WithResource("networkpolicies"): {
+			singular:   "networkpolicy",
+			namespaced: true,
+		},
+		verberClientTypeRbacGV.WithResource("clusterroles"): {
+			singular:   "clusterrole",
+			namespaced: false,
+		},
+		verberClientTypeRbacGV.WithResource("clusterrolebindings"): {
+			singular:   "clusterrolebinding",
+			namespaced: false,
+		},
+		verberClientTypeRbacGV.WithResource("roles"): {
+			singular:   "role",
+			namespaced: true,
+		},
+		verberClientTypeRbacGV.WithResource("rolebindings"): {
+			singular:   "rolebinding",
+			namespaced: true,
+		},
+		verberClientTypePluginsGV.WithResource("plugins"): {
+			singular:   "plugin",
+			namespaced: true,
+		},
+		// TODO custom resource via parsing definitions?
 	}
 )
 
@@ -120,6 +269,13 @@ func coreResourceNamespacedListHandlerFor(resource string) http.Handler {
 	})
 }
 
+func apiPrefix(gv schema.GroupVersion) string {
+	if gv.Group == "" {
+		return "/api/" + gv.Version
+	}
+	return "/apis/" + gv.Group + "/" + gv.Version
+}
+
 func main() {
 	logger := slog.Default()
 	ctx := context.Background()
@@ -182,36 +338,65 @@ func main() {
 		}),
 	)
 
-	for resource, kind := range coreNamespacedResources {
+	for resource := range coreNamespacedResources {
 		mux.Handle(
 			"/api/v1/namespaces/{namespace}/"+resource,
 			coreResourceNamespacedListHandlerFor(resource),
 		)
+	}
 
-		mux.Handle(
-			"/api/v1/namespaces/{namespace}/"+resource+"/{name}",
-			&httputil.ReverseProxy{
-				Transport: undashhttp.NoExplicitCompression(undashhttp.RequestLog(http.DefaultTransport)),
-				Rewrite: func(r *httputil.ProxyRequest) {
-					url, err := url.Parse(fmt.Sprintf(
-						"%s/api/v1/_raw/%s/namespace/%s/name/%s",
-						upstream,
-						kind.singular,
-						r.In.PathValue("namespace"),
-						r.In.PathValue("name"),
-					))
-					if err != nil {
-						panic(err)
-					}
+	// TODO advertise these at discovery
+	for gvr, meta := range verberMappedResources {
+		if meta.namespaced {
+			mux.Handle(
+				apiPrefix(gvr.GroupVersion())+"/namespaces/{namespace}/"+gvr.Resource+"/{name}",
+				&httputil.ReverseProxy{
+					Transport: undashhttp.NoExplicitCompression(undashhttp.RequestLog(http.DefaultTransport)),
+					Rewrite: func(r *httputil.ProxyRequest) {
+						url, err := url.Parse(fmt.Sprintf(
+							"%s/api/v1/_raw/%s/namespace/%s/name/%s",
+							upstream,
+							meta.singular,
+							r.In.PathValue("namespace"),
+							r.In.PathValue("name"),
+						))
+						if err != nil {
+							panic(err)
+						}
 
-					r.Out.URL = url
+						r.Out.URL = url
+					},
+					ModifyResponse: undashhttp.ChainModifyResponse(
+						undashhttp.ErrorResponseAsMetaV1Status,
+						undashhttp.RewriteObjectAsTableIfRequested,
+					),
 				},
-				ModifyResponse: undashhttp.ChainModifyResponse(
-					undashhttp.ErrorResponseAsMetaV1Status,
-					undashhttp.RewriteObjectAsTableIfRequested,
-				),
-			},
-		)
+			)
+		} else {
+			mux.Handle(
+				apiPrefix(gvr.GroupVersion())+"/"+gvr.Resource+"/{name}",
+				&httputil.ReverseProxy{
+					Transport: undashhttp.NoExplicitCompression(undashhttp.RequestLog(http.DefaultTransport)),
+					Rewrite: func(r *httputil.ProxyRequest) {
+						url, err := url.Parse(fmt.Sprintf(
+							"%s/api/v1/_raw/%s/name/%s",
+							upstream,
+							meta.singular,
+							r.In.PathValue("name"),
+						))
+						if err != nil {
+							panic(err)
+						}
+
+						r.Out.URL = url
+					},
+					ModifyResponse: undashhttp.ChainModifyResponse(
+						undashhttp.ErrorResponseAsMetaV1Status,
+						undashhttp.RewriteObjectAsTableIfRequested,
+					),
+				},
+			)
+		}
 	}
 
 	l, err := net.Listen("tcp", "localhost:9091")
